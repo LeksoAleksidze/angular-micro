1fa874ff9eeecd28ea65e192a08f7385e77b52d1
Laleksidze <laleksidze@crocobet.com>
2023-10-02 15:37:53 +0400

    feat: modify

--- apps/viking/src/app/remote-entry/nx-welcome.component.ts
+++ apps/viking/src/app/remote-entry/nx-welcome.component.ts
@@ -830,7 +830,7 @@ nx affected:e2e</pre>
           </details>
         </div>
         <p id="love">
-          Carefully crafted with
+          Carefully crafted with 222
           <svg
             fill="currentColor"
             stroke="none"
